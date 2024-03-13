f060eee39eb8b1e37b57f169b6dc16b3183f087a
Ben Bertka <bbertka@gmail.com>
2024-03-13 12:01:30 -0500

    Added a concurrent test case for simultaneus review processing

---
+++ test2.sh
@@ -0,0 +1,13 @@
+# Read the list of items from the file and execute the curl command concurrently
+cat items.txt | while IFS= read -r item; do
+  # URL encode the item (requires `jq` utility)
+  encoded_item=$(jq -nr --arg item "$item" '$item|@uri')
+  
+  # Execute the curl command
+  curl "http://192.168.1.110:5000/sentiment?item=${encoded_item}" &
+done
+
+# Wait for all background jobs to complete
+wait
+
+echo "All jobs completed."
